55ae7c2c29085b4c63397a67f1d26e99390c127f
Leandro <l.hernandez071999@gmail.com>
2023-06-09 12:50:19 -0300

    Arreglos

---
+++ javascriptPractica/JS/expresionesRegulares.js
@@ -0,0 +1,15 @@
+/*let valor=prompt("Ingrese un numero");
+let reg=new RegExp('[0-9]');
+
+if(reg.test(valor)){
+    alert("Se ingreso un numero");
+}else{
+    alert("No se ingreso un numero");
+}*/
+
+document.getElementById('formulario').addEventListener('submit',expresionRegular);
+
+function expresionRegular(evt){
+    document.getElementById('contador').innerHTML="putoxd";
+    evt.preventDefault();
+}
