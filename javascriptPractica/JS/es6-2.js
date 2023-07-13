294b561355feea1b9f5d3b1786ac606910c71424
Leandro Hernandez <lhernandez@lhernandez.168.1.10>
2023-07-13 00:34:58 -0300

    Se suben

--- javascriptPractica/JS/es6-2.js
+++ javascriptPractica/JS/es6-2.js
@@ -1,18 +1,18 @@
 //Declaracion de clases
 class Cliente {
-    //Constructor
-    constructor(nombre, saldo) {
-        this.nombre = nombre;
-        this.saldo = saldo;
-    }
-    //Atributos se acceden con la palabra clave 'this'
-    depositar(dinero) {
-        this.saldo = this.saldo + dinero;
-    }
+  //Constructor
+  constructor(nombre, saldo) {
+    this.nombre = nombre;
+    this.saldo = saldo;
+  }
+  //Atributos se acceden con la palabra clave 'this'
+  depositar(dinero) {
+    this.saldo = this.saldo + dinero;
+  }
 
-    extraer(dinero) {
-        this.saldo = this.saldo - dinero;
-    }
+  extraer(dinero) {
+    this.saldo = this.saldo - dinero;
+  }
 }
 
 /*
@@ -23,49 +23,69 @@ cliente1.depositar(200);
 cliente1.extraer(50);
 */
 
-
 class Menu {
-    constructor() {
-        this.url = [];
-        this.descripcion = [];
-    }
+  constructor() {
+    this.url = [];
+    this.descripcion = [];
+  }
 
-    insertarEnlace(urlEnlace, descripcionEnlace) {
-        this.url.push(urlEnlace);
-        this.descripcion.push(descripcionEnlace);
-    }
+  insertarEnlace(urlEnlace, descripcionEnlace) {
+    this.url.push(urlEnlace);
+    this.descripcion.push(descripcionEnlace);
+  }
 
-    mostrar(elemento) {
-        let cadena = '<div>';
-        for (let x = 0; x < this.url.length; x++) {
-            cadena += `[<a href="${this.url[x]}">${this.descripcion[x]}</a>]`;
-        }
-        cadena += '</div>';
-        document.getElementById(elemento).innerHTML = cadena;
+  mostrar(elemento) {
+    let cadena = "<div>";
+    for (let x = 0; x < this.url.length; x++) {
+      cadena += `[<a href="${this.url[x]}">${this.descripcion[x]}</a>]`;
     }
+    cadena += "</div>";
+    document.getElementById(elemento).innerHTML = cadena;
+  }
 }
 
-document.addEventListener('DOMContentLoaded', function() {
-    const menu = new Menu();
-    menu.insertarEnlace("https://www.example.com", "Enlace de ejemplo");
-    menu.insertarEnlace("https://www.example.com", "Enlace de ejemplo");
-    menu.insertarEnlace("https://www.example.com", "Enlace de ejemplo");
-    menu.mostrar("menuid");
+document.addEventListener("DOMContentLoaded", function () {
+  const menu = new Menu();
+  menu.insertarEnlace("https://www.example.com", "Enlace de ejemplo");
+  menu.insertarEnlace("https://www.example.com", "Enlace de ejemplo");
+  menu.insertarEnlace("https://www.example.com", "Enlace de ejemplo");
+  menu.mostrar("menuid");
 });
 
 //Herencia
-class Persona{
-    constructor(nombre,edad){
-        this.nombre=nombre;
-        this.edad=edad;
-    }
+class Persona {
+  constructor(nombre, edad) {
+    this.nombre = nombre;
+    this.edad = edad;
+  }
 
-    imprimir(){
-        document.write(`Nombre: ${this.nombre}`);
-        document.write(`Edad: ${this.edad}`);
-    }
+  imprimir() {
+    document.write(`Nombre: ${this.nombre}`);
+    document.write(`Edad: ${this.edad}`);
+  }
 }
 
-class Empleado extends Persona{
-    
+class Empleado extends Persona {
+  constructor(nombre, edad, sueldo) {
+    super(nombre, edad);
+    this.sueldo = sueldo;
+  }
+
+  imprimir() {
+    super.imprimir();
+    document.write(`Sueldo: ${this.sueldo}<br>`);
+  }
+
+  pagarImpuestos() {
+    if (this.sueldo > 5000) {
+      document.write(`${this.nombre} debe pagar impuestos <br>`);
+    } else {
+      document.write(`${this.nombre} no debe pagar impuestos <br>`);
+    }
+  }
 }
+
+const empleado1=new Empleado('Leandro','24','6000');
+empleado1.pagarImpuestos();
+
+
